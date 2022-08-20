168b31e3f2be475917c3fcc50e86fbd7e6a852a0
Lucas Guimarães <lukahbg@gmail.com>
2022-08-19 23:29:25 -0300

    [feat]: add app routes using go_router

---
+++ fale_mais_flutter/lib/router/routes.dart
@@ -0,0 +1,29 @@
+import 'package:fale_mais_flutter/resources/constants/route_names.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class MyRouter {
+  MyRouter();
+
+  late final router = GoRouter(
+    routes: [
+      GoRoute(
+        name: rootRouteName,
+        path: '/',
+        pageBuilder: (context, state) => MaterialPage<void>(
+          key: state.pageKey,
+          child: const HomePage(),
+        ),
+      ),
+    ],
+
+    // If no route was found or there's an error
+    // at the parameters
+    errorPageBuilder: (context, state) => MaterialPage<void>(
+      key: state.pageKey,
+      child: Scaffold(
+        body: Center(child: Text(state.error.toString())),
+      ),
+    ),
+  );
+}
