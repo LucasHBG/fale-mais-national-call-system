1ac02bbb9628b9ab6cae940a1793b6c65b90ec3f
Lucas Guimarães <lukahbg@gmail.com>
2022-08-25 00:54:49 -0300

    [feat]: add ReactiveFormsModule

--- fale_mais_frontend_angular/src/app/app.module.ts
+++ fale_mais_frontend_angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from "@angular/forms/";
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -10,7 +11,7 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
