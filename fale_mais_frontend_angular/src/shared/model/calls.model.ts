6a195934f1904310c97ff4d3ecfcf2a92dd3bbc5
Lucas Guimarães <lukahbg@gmail.com>
2022-08-28 22:33:47 -0300

    [feat]: add Calls model

---
+++ fale_mais_frontend_angular/src/shared/model/calls.model.ts
@@ -0,0 +1,12 @@
+import { Plans } from "./plans.model";
+
+export interface Calls {
+    id?: string,
+    origin: number,
+    destiny: number,
+    minutes: number,
+    plan: Plans,
+    withPlan: number,
+    noPlan: number,
+}
+
