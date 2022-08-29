84ef192414c9975b9079ba4d2fb9698cd26fe3cc
Lucas Guimarães <lukahbg@gmail.com>
2022-08-29 01:26:45 -0300

    [feat]: added new components

--- fale_mais_frontend_angular/src/app/app.module.ts
+++ fale_mais_frontend_angular/src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from "@angular/forms/";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { CallsComponent } from './home/calls/calls.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CallsComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
