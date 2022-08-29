f3ea3ac22b412cb52d5a49c7d944385319c5155b
Lucas Guimarães <lukahbg@gmail.com>
2022-08-29 01:30:36 -0300

    [feat]: added getters for form, added Calls and Plans services

--- fale_mais_frontend_angular/src/app/home/home.component.ts
+++ fale_mais_frontend_angular/src/app/home/home.component.ts
@@ -1,19 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CallsService } from 'src/service/calls/calls.service';
+import { PlansService } from 'src/service/plans/plans.service';
+import { Plans } from 'src/shared/model/plans.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
+  providers: [PlansService, CallsService],
 })
 export class HomeComponent implements OnInit {
-  callSimulatorForm: FormGroup = new FormGroup({
-    origin: new FormControl(''),
-    destiny: new FormControl(''),
-    minutes: new FormControl(''),
+  public isSubmitted: boolean = false;
+  public plans: Plans[] = [];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private plansService: PlansService,
+    private callsService: CallsService
+  ) {}
+
+  public callSimulatorForm: FormGroup = this.formBuilder.group({
+    origin: ['', [Validators.required, Validators.maxLength(3)]],
+    destiny: ['', [Validators.required, Validators.maxLength(3)]],
+    minutes: ['', [Validators.required, Validators.maxLength(5)]],
+    plan: ['FaleMais 30'],
   });
 
-  constructor() {}
+  get origin() {
+    return this.callSimulatorForm.get('origin');
+  }
 
-  ngOnInit(): void {}
+  get destiny() {
+    return this.callSimulatorForm.get('destiny');
+  }
+
+  get minutes() {
+    return this.callSimulatorForm.get('minutes');
+  }
+
+  get plan() {
+    return this.callSimulatorForm.get('plan');
+  }
+
+  ngOnInit(): void {
+    this.plansService.getPlans().then((plans) => (this.plans = plans));
+  }
+
+  onSubmit() {
+    this.isSubmitted = true;
+    console.log(this.callSimulatorForm.value);
+    this.callsService.saveCalls(this.callSimulatorForm.value);
+  }
 }
