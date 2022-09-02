7d74e2f7c6a9b6d5dbcc47f2a2ca7972b60442c7
Lucas Guimarães <lukahbg@gmail.com>
2022-09-01 23:22:49 -0300

    [feat]: add variables for some widgets

--- fale_mais_flutter/lib/view/widgets/number_only_input_box.dart
+++ fale_mais_flutter/lib/view/widgets/number_only_input_box.dart
@@ -3,25 +3,31 @@ import 'package:flutter/services.dart';
 
 import '/resources/app_context_extension.dart';
 
-class NumberOnlyInputBox extends StatelessWidget {
-  const NumberOnlyInputBox({
+class NumberOnlyRoundedInputBox extends StatelessWidget {
+  const NumberOnlyRoundedInputBox({
     Key? key,
     required this.context,
     required this.controller,
-    this.maxLengthTextInput,
+    this.maxLengthTextInput = 3,
+    this.inputWidth = 75.0,
+    this.inputHeight = 75.0,
+    this.inputFontSize = 40.0,
   }) : super(key: key);
 
   final BuildContext context;
   final TextEditingController controller;
-  final int? maxLengthTextInput;
+  final int maxLengthTextInput;
+  final double inputWidth;
+  final double inputHeight;
+  final double inputFontSize;
 
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(10.0),
       child: Container(
-        width: 75,
-        height: 75,
+        width: inputWidth,
+        height: inputHeight,
         color: context.resources.color.colorNeutral,
         padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
         child: TextField(
@@ -29,15 +35,14 @@ class NumberOnlyInputBox extends StatelessWidget {
           keyboardType: TextInputType.number,
           textAlign: TextAlign.center,
           controller: controller,
-          style: const TextStyle(
-            fontSize: 40,
+          style: TextStyle(
+            fontSize: inputFontSize,
             fontWeight: FontWeight.w500,
           ),
           inputFormatters: <TextInputFormatter>[
             FilteringTextInputFormatter.digitsOnly,
             // Brazil's highest DDD number is 99
-            if (maxLengthTextInput != null)
-              LengthLimitingTextInputFormatter(maxLengthTextInput),
+            LengthLimitingTextInputFormatter(maxLengthTextInput),
           ],
         ),
       ),
