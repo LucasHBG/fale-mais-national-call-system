6155b3f29ac6b928bcc911c43b5f666d449ce330
Lucas Guimarães <lukahbg@gmail.com>
2022-09-05 23:38:06 -0300

    [chore]: added comments to method getPlans()
    Co-authored-by: Gabriel Alves dos Santos <gabriel.alves.santos@philips.com>

--- fale_mais_frontend_angular/src/service/plans/plans.service.ts
+++ fale_mais_frontend_angular/src/service/plans/plans.service.ts
@@ -5,7 +5,11 @@ import { PLANS } from 'src/shared/mock-plans';
 @Injectable()
 export class PlansService {
   
-  // Get all Plans from db
+  /**
+   * Get all `Plans` objects from db(not implemented) or mocked-plans list
+   * 
+   * @returns Promise with current values from mocked-calls list
+   */
   getPlans() {
     return Promise.resolve(PLANS);
   }
